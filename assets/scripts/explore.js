fed67de4c6d0585c9092abdfbff5fce0ea9aefa6
erw004 <156253931+erw004@users.noreply.github.com>
2025-05-06 21:20:37 -0700

    speech

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,41 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectVoice = document.getElementById("voice-select");
+  const talkButton = document.querySelector("button");
+  const textToSpeak = document.getElementById("text-to-speak");
+
+  const smilingFace = document.querySelector("img");
+
+  const synth = window.speechSynthesis;
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    selectVoice.innerHTML = '';
+
+    voices.forEach((voice, i) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = i;
+      selectVoice.appendChild(option);
+    });
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  talkButton.addEventListener('click', function() {
+    const utterance = new SpeechSynthesisUtterance(textToSpeak.value);
+    utterance.voice = voices[selectVoice.value];
+
+    smilingFace.src = 'assets/images/smiling-open.png';
+    utterance.onend = () => {
+      smilingFace.src = 'assets/images/smiling.png';
+    };
+    synth.speak(utterance);
+  })
 }
